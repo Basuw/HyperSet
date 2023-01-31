8a953d63ef59a4b30325da190174ebef09a5e32d
aurian.jault <aurian.jault@etu.uca.fr>
2023-01-31 17:23:50 +0100

    pseudo code du 1er algo

---
+++ src/algo.js
@@ -0,0 +1,18 @@
+// Pseudo code
+//
+/*
+ * function checkReslt(array <Cards>)
+ * {
+ *      new array<array<Attrib>> matrice = vide
+ *
+ *      forEach(array: value)
+ *      {
+ *          matrice.add(value.getAttrib)
+ *      }
+ *      Check all length of matrice
+ *      for(i = 0; i<matrice[0])
+ *
+ * }
+ *
+ *
+ * */ 
