4b77c1f3bdb7349bbd7dde5fa23c9d013adc76a1
RemRem <remrem@github.com>
2023-03-06 16:54:49 +0100

    fuck createPath func works (at 70%)

--- src/Model/card-to-html.js
+++ src/Model/card-to-html.js
@@ -1,46 +1,60 @@
 class CardToHtml {
+  
   static create(card) {
     const gameWindow = document.querySelector('#main');
-    let div = document.createElement('div');
+    const div = document.createElement('div');
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
     div.setAttribute('class','card');
-    gameWindow.appendChild(div);
-
-    const svg = this.createPath(card.shape,card.color,card.filling,card.outline);
-
-    div.appendChild(svg);
-  }
-
-  createSVG(shape,color,filling,outline) {
-    if(shape === null) shape = "oval";
-    if(color === null) color = "#000000";
-    if(filling === null) filling = "fill";
-
-    // Can be optimized by just adding svg as string -> svg is constant
-    // class="item" height="160" width="80" viewBox="0 0 200 400"
-    const svg = document.createElement('svg');
-    svg.setAttribute("class","item");
-    svg.setAttribute("height","160");
-    svg.setAttribute("width","80");
-    svg.setAttribute("viewBox","0 0 200 400");
-
-    const path = document.createElement('path');
-
-    path.setAttribute("d",SHAPE_PATH[shape]);
-
-    if(outline !== null){
-      Object.keys(OUTLINE_SPEC[outline]).forEach(function(k){
-        path.setAttribute(k,OUTLINE_SPEC[outline[k]]);
-      });
-
-      path.setAttribute("stroke","#000000");
+    svg.setAttribute('class','item');
+    svg.setAttribute('height','160');
+    svg.setAttribute('width','80');
+    svg.setAttribute('viewBox','0 0 200 400');
+    
+    // Create paths + add to svg
+    for(let j = 0; j < 2; j++) {
+      const path = this.createPath(card.shape,card.color,card.filling,card.outline,j);
+      svg.appendChild(path);
     }
 
-    path.setAttribute("stroke",color);
-    path.setAttribute("fill",color);
-    path.setAttribute("mask","none");
+    // Loop to add svg card.number times
+    for(let i = 0; i < card.number; i++) {
+      div.appendChild(svg.cloneNode(true))
+      console.log("+SVG: ",svg);
+    }
 
-    svg.appendChild(path);
+    gameWindow.appendChild(div);
+  }
 
-    return svg;
+  static createPath(shape,color,filling,outline,step) {
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+    if(shape === null) shape = 'oval';
+    if(color === null) color = '000000';
+    if(filling === null) filling = 'fill';
+
+    // Add lots of attributes
+    path.setAttribute("d",SHAPE_PATH[shape]);
+    path.setAttribute('stroke',`#${color}`);
+    path.setAttribute('fill',`#${color}`);
+    path.setAttribute('stroke-width','18');
+
+    // Add svg attributes for shape outline
+    if(outline !== null){
+      Object.keys(OUTLINE_SPEC.rect).forEach(function(k){
+        path.setAttribute(k,OUTLINE_SPEC.rect[k]);
+      });
+
+      path.setAttribute('stroke','#000000');
+    }
+
+    // Diff between two paths in svg
+    if (step === 0) {
+      path.setAttribute('mask',`url(#mask-${filling})`);
+    } else {
+      path.setAttribute('fill', 'none');
+    }
+
+    return path;
   }
 }
