7ee4d1777675bcb1ff95dcf038b3179fdbaed95a
Bastien Jacquelin <bastien.jacquelin@etu.uca.fr>
2023-03-13 15:13:52 +0100

    module card & deck

---
+++ src/Model/card-module.js
@@ -0,0 +1,16 @@
+export default{
+    emits:[],
+    data: function(){
+        return{
+
+        }
+    },
+    methods:{
+        clicked:function () {
+            
+        }
+    },
+    template:`
+    <div v-bind:style="{ color: 'red', fontSize: '20px' }">yess</div>
+    `
+}
---
+++ src/Model/deck-module.js
@@ -0,0 +1,21 @@
+export default{
+    emits:[],
+    data: function(){
+        return{
+            cards:[]
+        }
+    },
+    methods:{
+        clicked:function () {
+            
+        }
+    },
+    template:`
+    <div v-bind:style="{border: '3px solid black', fontSize: '20px', display:'flex'}">
+        <div v-for="n in 10">
+            {{ n }}
+            <card-module></card-module>
+        </div>
+    </div>
+    `
+}
