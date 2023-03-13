ef22a1ab3d78d309f4323757c3b6faf106704dc9
Bastien Jacquelin <bastien.jacquelin@etu.uca.fr>
2023-03-13 15:32:31 +0100

    affichage des différentes cartes

--- src/Model/card-module.js
+++ src/Model/card-module.js
@@ -11,6 +11,6 @@ export default{
         }
     },
     template:`
-    <div v-bind:style="{ color: 'red', fontSize: '20px' }">yess</div>
+    <div v-bind:style="{margin:'20px', border: '2px solid black', color: 'red', fontSize: '20px' }">yess</div>
     `
 }
--- src/Model/deck-module.js
+++ src/Model/deck-module.js
@@ -11,7 +11,7 @@ export default{
         }
     },
     template:`
-    <div v-bind:style="{border: '3px solid black', fontSize: '20px', display:'flex'}">
+    <div v-bind:style="{border: '3px solid black', fontSize: '20px', display:'flex', margin:'20px 10em 20px 20px'}">
         <div v-for="n in 10">
             {{ n }}
             <card-module></card-module>
