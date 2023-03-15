acd360740d1371599cefddc296946d36b9fb1ef4
Bastien Jacquelin <bastien.jacquelin@etu.uca.fr>
2023-03-15 14:27:31 +0100

    add deck to view, can click on cards

--- views/deck-module.js
+++ views/deck-module.js
@@ -2,19 +2,26 @@ export default{
     emits:[],
     data: function(){
         return{
-            card:new Card({"filling":"empty"})
+            card:new Card({"filling":"empty"}),
+            deck : new Deck([0,1,2,3],3),
+            id:0,
+            selectedCards:[]
         }
     },
     methods:{
-        clicked:function () {
-            
-        }
+        selected(id){
+            console.log("selected",id);
+            this.selectedCards[id]=this.deck.outputCards[id-1]
+        },
+        unSelected(id){
+            console.log("unSelected",id);
+        },
     },
     template:`
     <div v-bind:style="{border: '3px solid black', fontSize: '20px', display:'flex', margin:'20px 10em 20px 20px'}">
-        <div v-for="n in 10">
+        <div v-for="n in deck.outputCards.length">
             {{ n }}
-            <card-module :card={card}></card-module>
+            <card-module @selected='selected' @un-selected='unSelected' :id=n :card=this.deck.outputCards[n-1]></card-module>
         </div>
     </div>
     `
