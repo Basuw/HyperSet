b478e374d5a8949a7bf1691551c8c1db9a3c512b
bajacqueli <bastien.jacquelin@etu.uca.fr>
2023-03-21 22:44:32 +0100

    bug to fix : add card when no set, only add 1 sometime

--- views/card-module.js
+++ views/card-module.js
@@ -9,16 +9,17 @@ export default{
     },
     methods:{
         clicked() {
-            if(!this.isClicked){
-                this.isClicked=true
-                document.querySelector(`#${this.idUsed}`).setAttribute("style","border: 2px solid red; margin: 20px; color: red; fontSize: 20px; cursor: pointer");
-                this.$emit('selected',this.id);
-            }
-            else{
-                this.isClicked=false
-                document.querySelector(`#${this.idUsed}`).setAttribute("style","border: 2px solid black; margin: 20px; color: red; fontSize: 20px; cursor: pointer");
-                this.$emit('unSelected',this.id);
-            }
+            this.$emit('selected',this.id);
+            // if(!this.isClicked){
+            //     this.isClicked=true
+            //     document.querySelector(`#${this.idUsed}`).setAttribute("style","border: 2px solid red; margin: 20px; color: red; fontSize: 20px; cursor: pointer");
+            //     this.$emit('selected',this.id);
+            // }
+            // else{
+            //     this.isClicked=false
+            //     document.querySelector(`#${this.idUsed}`).setAttribute("style","border: 2px solid black; margin: 20px; color: red; fontSize: 20px; cursor: pointer");
+            //     this.$emit('unSelected',this.id);
+            // }
         }
     },
     template:`
--- views/deck-module.js
+++ views/deck-module.js
@@ -12,37 +12,36 @@ export default{
     methods:{
         selected(id){
             if(this.nbCardsSelected>=this.deck.nbCards){
-                document.querySelector(`#id${id}`).setAttribute("style","border: 2px solid black; margin: 20px; color: red; fontSize: 20px; cursor: pointer");
+                this.set();
+                console.log("deb")
             }
             else{
-                this.nbCardsSelected+=1;
-                console.log("this.nbCardsSelected",this.nbCardsSelected);
-                console.log("selected",id);
-                this.selectedCards[id]=this.deck.outputCards[id-1]
-                console.log('selectedCards add',this.selectedCards);
-                if(this.nbCardsSelected==this.deck.nbCards){
-                    this.set();
-                    console.log("dqsdgqhj")
+                if(this.selectedCards[id]!=null){
+                    console.log("deselec")
+                    document.querySelector(`#id${id}`).setAttribute("style","border: 2px solid black; margin: 20px; color: red; fontSize: 20px; cursor: pointer");
+                    this.nbCardsSelected-=1
+                    this.selectedCards[id]=null
                 }
-            }
-        },
-        unSelected(id){
-            if(this.selectedCards[id]!=null){
-                this.selectedCards.splice(id,1);
-                this.nbCardsSelected-=1;
-                console.log("this.nbCardsSelected",this.nbCardsSelected);
-                console.log('selectedCards remove',this.selectedCards);
-                console.log("unSelected",id);
+                else{
+                    console.log("Selec")
+                    this.selectedCards[id]=this.deck.outputCards[id-1]
+                    document.querySelector(`#id${id}`).setAttribute("style","border: 2px solid red; margin: 20px; color: red; fontSize: 20px; cursor: pointer");
+                    this.nbCardsSelected+=1
+                    if(this.nbCardsSelected==this.deck.nbCards){
+                        console.log("this.selectedCards.length",this.selectedCards.length)
+                        this.set();
+                    }
+                }
             }
         },
         set(){
             console.log("Check Set")
             let checkSet=true;
             if(checkSet){
+                console.log("this.selectedCards.length",this.selectedCards.length)
                 this.deck.checkSet(this.selectedCards);
                 this.nbCardsSelected=0;
                 this.selectedCards.splice(0,this.selectedCards.length+1)
-                console.log("this.selectedCards.length",this.selectedCards.length)
             }
         },
     },
@@ -50,7 +49,7 @@ export default{
     <div v-bind:style="{border: '3px solid black', fontSize: '20px', display:'flex', margin:'20px 10em 20px 20px'}">
         <div v-for="n in deck.outputCards.length">
             {{ n }}
-            <card-module @selected='selected' @un-selected='unSelected' :id=n :card=this.deck.outputCards[n-1]></card-module>
+            <card-module @selected='selected' :id=n :card=this.deck.outputCards[n-1]></card-module>
         </div>
     </div>
     `
