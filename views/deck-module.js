cc4eebe0ab98d172cc68f6da280bbf0872974bc7
remi.arnal <remi.arnal@etu.uca.fr>
2023-04-02 19:40:29 +0200

    zoomed form in cards and center game div

--- views/deck-module.js
+++ views/deck-module.js
@@ -9,7 +9,7 @@ export default{
         return{
             card:new Card({"filling":"empty"}),
             id:0,
-            deck : new Deck([0,1,2,3],4),
+            deck : new Deck([0,1,2],3),
             selectedCards:[],
             selectedCardsindex:[],
             nbCardsSelected:0,
@@ -65,7 +65,7 @@ export default{
         <h2>Players: {{connected}}</h2>
     </div>
     
-    <div v-if="!win" v-bind:style="{border: '3px solid black', fontSize: '20px', display:'flex', 'flex-wrap':'wrap',margin:'1rem 20rem 20px 20px'}">
+    <div v-if="!win" v-bind:style="{border: '3px solid black', fontSize: '20px', display:'flex', 'flex-wrap':'wrap',margin:'1rem 17vw 20px 17vw'}">
             <card-module @selected='selected' :id=n :card=this.deck.outputCards[n-1] v-for="n in deck.outputCards.length"/>
     </div>
     <div  v-if="win">
